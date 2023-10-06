57480cdd6635bc02b24875cbeb17f2507bc767a1
Stifler00 <72310729+Stifler00@users.noreply.github.com>
2023-10-06 22:34:39 +0200

    Create index.php

---
+++ Zadatak1/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="hr">
+    <head>
+        <title> Zadatak 1</title>
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Jan Tovernić">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+    </head>
+
+    <body>
+
+    </body>
+</html>
